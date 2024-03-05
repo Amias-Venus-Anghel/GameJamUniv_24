6158835df3a552d28b71dabcb88de042343b802c
Venus-Panda <panda.anghel01@gmail.com>
2024-03-05 21:03:08 +0200

    added hexagon placement mechanic
    to do: chnage parent to world view canvas for camera movement in the future

---
+++ GameJam_Univ/Assets/Scripts/GenerateDeck.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateDeck : MonoBehaviour
+{
+    [SerializeField] int cardsNumber = 10;
+    [SerializeField] GameObject prefabCard;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cardsNumber; i++) {
+            GameObject card = Instantiate(prefabCard, transform.position, Quaternion.identity);
+            card.transform.SetParent(transform.parent);
+        }
+    }
+}

afc3d9b45e0071eaa0804a1abd5de4123e9322d8
maria-alexandru <mm.maria.alexandru@gmail.com>
2024-03-07 18:48:55 +0200

    attack

---
+++ GameJam_Univ/Assets/Scripts/Attack.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject target = null;
+    private GameObject projectile;
+    private float range = 150;
+    float time = 0;
+    float fireTime = 0.6f;
+
+   [SerializeField] private LayerMask enemyMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+        } 
+        else
+        {
+            if (TargetInRange(target) == false)
+                target = null;
+
+            time += Time.deltaTime;
+            if (time > fireTime)
+            {
+                time = 0;
+                SpawnProjectile();
+
+            }
+        }
+    }
+
+    void SpawnProjectile()
+    {
+        projectile = Instantiate(projectilePrefab);
+        projectile.transform.SetParent(transform.parent);
+        projectile.transform.position = transform.position;
+        if(target != null)
+        {
+            projectile.GetComponent<Projectile>().SetTarget(target);
+            projectile.GetComponent<Projectile>().SetRange(range);
+            projectile.GetComponent<Projectile>().SetInitPos(transform.position);
+        }
+    }
+
+    void FindTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemies.Length; i++) {
+            if (TargetInRange(enemies[i]) == true)
+                target = enemies[i];
+        }
+    }
+
+    bool TargetInRange(GameObject Target)
+    {
+        return (Vector2.Distance(Target.transform.position, transform.position) <= range);
+    }
+}
---
+++ GameJam_Univ/Assets/Scripts/Projectile.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    GameObject target = null;
+    float speed = 0.3f;
+    float range;
+    Vector3 initiPos;
+    public float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+            Destroy(this.gameObject);
+        else
+        {
+            if (TargetInRange() == false)
+                Destroy(this.gameObject);
+
+            if (transform.position != target.transform.position)
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+
+            if (Mathf.Abs(transform.position.x - target.transform.position.x) <= 0.05f &&
+                Mathf.Abs(transform.position.y - target.transform.position.y) <= 0.05f)
+                Destroy(this.gameObject);
+        }
+    }
+
+    bool TargetInRange()
+    {
+        distance = Vector2.Distance(initiPos, target.transform.position);
+        if (Vector2.Distance(initiPos, target.transform.position) <= range)
+            return true;
+        return false;
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+
+    public void SetRange(float range)
+    {
+        this.range = range;
+    }
+
+    public void SetInitPos(Vector3 pos)
+    {
+        this.initiPos = pos;
+    }
+}

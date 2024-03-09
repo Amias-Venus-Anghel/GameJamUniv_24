a3f6992ff7b731ff37d686d7519680296180c912
Venus-Panda <panda.anghel01@gmail.com>
2024-03-09 19:27:24 +0200

    added script to check loop and reset

---
+++ GameJam_Univ/Assets/Scripts/Hexagons/CheckRoadLoop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CheckRoadLoop : MonoBehaviour
+{
+    private List<GameObject> roadPlacers = null;
+    private HexagonDragDrop endPoint = null;
+    private GameMaster gameMaster = null;
+
+    void Start() {
+        roadPlacers = new List<GameObject>();
+        endPoint = GameObject.FindGameObjectWithTag("Endpoint").GetComponent<HexagonDragDrop>();
+        gameMaster = GetComponent<GameMaster>();
+    }
+
+    void Update() {
+        roadPlacers = GameObject.FindGameObjectsWithTag("RoadPlace").ToList<GameObject>();
+        // if can't place anymore => a loop has occured
+        if (roadPlacers.Count <= 0 && !endPoint.placed) {
+            Debug.Log("loop");
+            gameMaster.NewRound();
+        }
+    }
+}

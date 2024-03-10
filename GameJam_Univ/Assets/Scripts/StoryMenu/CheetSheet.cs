2c14de3dce4d1c24b61825e5d76e19bf90f21956
Venus-Panda <panda.anghel01@gmail.com>
2024-03-10 14:19:02 +0200

    added cheet sheet

---
+++ GameJam_Univ/Assets/Scripts/StoryMenu/CheetSheet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheetSheet : MonoBehaviour
+{
+    [SerializeField] Sprite[] sheets;
+    private Image image;
+    private int index = 0;
+
+    void Start() {
+        image = GetComponent<Image>();
+        image.sprite = sheets[0];
+        gameObject.SetActive(false);
+    } 
+
+    public void NextSheet() {
+        index ++;
+        if (index >= sheets.Length) {
+            index = 0;
+        }
+        image.sprite = sheets[index];
+    }
+
+    public void CloseOpen() {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+    
+}
